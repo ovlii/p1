84b4a7b45b8dd292509a5cc31655f6e80a9a534c
ovlii <vannangi@g.harvard.edu>
2014-09-16 19:11:57 -0400

    Initial Creation

--- includes/nav.php
+++
@@ -1,9 +0,0 @@
-<div id="menubar">
-   <ul id="menu">  
-      <!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
-      <li id="navpart1"><a href="index.php">Project 1</a></li>
-      <li id="navpart2"><a href="#">Project 2</a></li>
-      <li id="navpart3"><a href="#">Project 3</a></li>
-      <li id="navpart4"><a href="#">Project 4</a></li>
-   </ul>
-</div>
